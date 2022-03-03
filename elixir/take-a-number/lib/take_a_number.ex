da2358307bf5028ce87541c56c2f5ef3ef9e3d1a
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-03-03 13:08:58 -0300

    elixir track - take-a-number

---
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -0,0 +1,27 @@
+defmodule TakeANumber do
+  def start() do
+    spawn(fn -> machine_state(0) end)
+  end
+
+  def machine_state(state) do
+    state =
+      receive do
+        {:report_state, sender_pid} ->
+          send(sender_pid, state)
+          state
+
+        {:take_a_number, sender_pid} ->
+          state = state + 1
+          send(sender_pid, state)
+          state
+
+        :stop ->
+          exit(:exit)
+
+        _ ->
+          state
+      end
+
+    machine_state(state)
+  end
+end
