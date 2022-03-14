9ed084c76997f6cdb7138ea7a02007bf30b1971b
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-03-14 12:01:06 -0300

    elixir track - rpg-character-sheet

---
+++ elixir/rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -0,0 +1,41 @@
+defmodule RPG.CharacterSheet do
+  def welcome() do
+    IO.puts("Welcome! Let's fill out your character sheet together.")
+
+    :ok
+  end
+
+  def ask_name() do
+    IO.gets("What is your character's name?\n")
+    |> String.trim()
+  end
+
+  def ask_class() do
+    IO.gets("What is your character's class?\n")
+    |> String.trim()
+  end
+
+  def ask_level() do
+    IO.gets("What is your character's level?\n")
+    |> String.trim()
+    |> String.to_integer()
+  end
+
+  def run() do
+    welcome()
+
+    character_name = ask_name()
+    character_class = ask_class()
+    character_level = ask_level()
+
+    character = %{
+      name: character_name,
+      class: character_class,
+      level: character_level
+    }
+
+    IO.inspect(character, label: "Your character")
+
+    character
+  end
+end
