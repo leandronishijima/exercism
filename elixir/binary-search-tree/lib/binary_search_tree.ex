bcc8cc8860bbc5fa57c91af15d53e2051cfe96ed
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-09-05 23:29:08 -0300

    elixir track - binary_search_tree

---
+++ elixir/binary-search-tree/lib/binary_search_tree.ex
@@ -0,0 +1,46 @@
+defmodule BinarySearchTree do
+  @type bst_node :: %{data: any, left: bst_node | nil, right: bst_node | nil}
+
+  @doc """
+  Create a new Binary Search Tree with root's value as the given 'data'
+  """
+  @spec new(any) :: bst_node
+  def new(data), do: %{data: data, left: nil, right: nil}
+
+  @doc """
+  Creates and inserts a node with its value as 'data' into the tree.
+  """
+  @spec insert(bst_node, any) :: bst_node
+  def insert(%{data: tree_data} = tree, data) when tree_data >= data,
+    do: insert_new_node(tree, :left, data)
+
+  def insert(tree, data),
+    do: insert_new_node(tree, :right, data)
+
+  defp insert_new_node(tree, node, data) do
+    if is_nil(tree[node]),
+      do: Map.replace(tree, node, new(data)),
+      else: Map.update(tree, node, data, &insert(&1, data))
+  end
+
+  @doc """
+  Traverses the Binary Search Tree in order and returns a list of each node's data.
+  """
+  @spec in_order(bst_node) :: [any]
+  def in_order(tree) do
+    in_order_reduce(tree, [])
+  end
+
+  defp in_order_reduce(%{data: data, left: nil, right: nil}, _acc),
+    do: [data]
+
+  defp in_order_reduce(%{data: data, left: left, right: nil}, acc) when not is_nil(left),
+    do: acc ++ in_order_reduce(left, acc) ++ [data]
+
+  defp in_order_reduce(%{data: data, left: nil, right: right}, acc) when not is_nil(right),
+    do: acc ++ [data] ++ in_order_reduce(right, acc)
+
+  defp in_order_reduce(%{data: data, left: left, right: right}, acc)
+       when not is_nil(right) and not is_nil(left),
+       do: acc ++ in_order_reduce(left, acc) ++ [data] ++ in_order_reduce(right, acc)
+end
