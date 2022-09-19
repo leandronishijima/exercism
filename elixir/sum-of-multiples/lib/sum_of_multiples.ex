ff7e05b17744ace07782cf3a8550f7cd0d8f7502
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-09-18 22:08:45 -0300

    elixir track - sum-of-multiple

---
+++ elixir/sum-of-multiples/lib/sum_of_multiples.ex
@@ -0,0 +1,19 @@
+defmodule SumOfMultiples do
+  @doc """
+  Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
+  """
+  @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
+  def to(limit, factors) do
+    1..(limit - 1)
+    |> Enum.filter(
+      &Enum.any?(factors, fn
+        0 ->
+          false
+
+        factor ->
+          rem(&1, factor) == 0
+      end)
+    )
+    |> Enum.sum()
+  end
+end
