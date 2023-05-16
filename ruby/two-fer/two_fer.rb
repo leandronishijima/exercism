701ba835434fb22c0433f4ee4d353dfc71d88fc2
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2023-05-16 11:19:36 -0300

    ruby track - saving-accounts

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,7 +1,9 @@
-class TwoFer
+module TwoFer
   def self.two_fer(name = nil)
-    return 'One for you, one for me.' if name.nil?
-
-    "One for #{name}, one for me."
+    if name.nil?
+      'One for you, one for me.'
+    else
+      "One for #{name}, one for me."
+    end
   end
 end
