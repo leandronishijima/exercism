42f63cb302e6a20835fcc8d404d2403a412a4a0f
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2023-06-17 09:41:13 -0300

    ruby track - high-scores

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,28 @@
+# Write your code for the 'High Scores' exercise in this file. Make the tests in
+# `high_scores_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/high-scores` directory.
+class HighScores
+  def initialize(scores)
+    @scores = scores
+  end
+
+  attr_reader :scores
+
+  def latest
+    @scores.last
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    @scores.max(3)
+  end
+
+  def latest_is_personal_best?
+    latest == personal_best
+  end
+end
